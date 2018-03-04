391c18186e03778da73839d3870852b20b985d45
Jonas Luebbers <jonaskluebbers@gmail.com>
2018-03-03 22:49:11 -0500

    Animate light without mouse

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const lightSource = document.querySelector('#light-source')
 
-window.addEventListener('mousemove', (event) => {
-    lightSource.setAttribute('cx', event.clientX)
-    lightSource.setAttribute('cy', event.clientY)
-})
-
+const vw = window.innerWidth
+const vh = window.innerHeight
 
 function draw() {
     const time = performance.now()
-    const radius = 100 + (noise.perlin2(time / 100, 0) * 5)
+    const x = ((Math.sin(time * 0.00055) * 0.4) + 0.5) * vw
+    const y =  ((noise.simplex2(time * 0.0001, 1000) * 0.4) + 0.5) * vh
+    const radius = 100 + (noise.perlin2(time * 0.001, 0) * 5)
+
+    lightSource.setAttribute('cx', x)
+    lightSource.setAttribute('cy', y)
     lightSource.setAttribute('r', radius)
 
     requestAnimationFrame(draw)
