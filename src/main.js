4766ef30297027f2d1bde6f6ffdc07c178855217
Jonas Luebbers <jonaskluebbers@gmail.com>
2018-03-12 00:34:23 -0400

    Replace three and noise with modules

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
+import * as three from 'three'
+import SimplexNoise from 'simplex-noise'
 import vertexShader from './vertex.glsl'
 import fragmentShader from './fragment.glsl'
+
+const simplex = new SimplexNoise()
 
 const vw = window.innerWidth
 const vh = window.innerHeight
 
-const scene = new THREE.Scene()
-const camera = new THREE.Camera()
+const scene = new three.Scene()
+const camera = new three.Camera()
 camera.position.z = 1
 
 const uniforms = {
@@ -15,20 +19,20 @@ const uniforms = {
     },
     resolution: {
         type: "v2",
-        value: new THREE.Vector2(vw, vh)
+        value: new three.Vector2(vw, vh)
     }
 }
 
-const material = new THREE.ShaderMaterial({
+const material = new three.ShaderMaterial({
     uniforms,
     vertexShader,
     fragmentShader
 })
 
-const mesh = new THREE.Mesh( new THREE.PlaneGeometry(2, 2), material)
+const mesh = new three.Mesh( new three.PlaneGeometry(2, 2), material)
 scene.add(mesh)
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new three.WebGLRenderer()
 renderer.setSize(vw, vh)
 document.body.appendChild(renderer.domElement)
 
@@ -37,8 +41,8 @@ function draw() {
 
     const time = performance.now()
     const x = ((Math.sin(time * 0.00055) * 0.4) + 0.5) * vw
-    const y =  ((noise.simplex2(time * 0.0001, 1000) * 0.4) + 0.5) * vh
-    const radius = 100 + (noise.perlin2(time * 0.001, 0) * 5)
+    const y =  ((simplex.noise2D(time * 0.0001, 1000) * 0.4) + 0.5) * vh
+    const radius = 100 + (simplex.noise2D(time * 0.001, 0) * 5)
 
     uniforms.time.value = performance.now()
     renderer.render(scene, camera)
