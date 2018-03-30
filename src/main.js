329a455f0025d396de0a77d4420169c5459d3a5b
Jonas Luebbers <jonaskluebbers@gmail.com>
2018-03-29 22:46:41 -0400

    Render screen and buttefly with separate cameras

--- src/main.js
+++ src/main.js
@@ -10,10 +10,15 @@ let vw = window.innerWidth
 let vh = window.innerHeight
 let aspect = vw / vh
 
-const scene = new three.Scene()
-const camera = new three.PerspectiveCamera(9, aspect, 0.1, 1000)
-camera.position.z = 10
-scene.add(camera)
+const perspectiveScene = new three.Scene()
+const perspectiveCamera = new three.PerspectiveCamera(30, aspect, 0.1, 1000)
+perspectiveCamera.position.z = 3
+perspectiveScene.add(perspectiveCamera)
+
+const orthographicScene = new three.Scene()
+const orthographicCamera = new three.OrthographicCamera(-1, 1, 1, -1, 0.1, 10)
+orthographicCamera.position.z = 1
+orthographicScene.add(orthographicCamera)
 
 const wingTexture = new three.TextureLoader().load('test.png')
 
@@ -40,28 +45,23 @@ const screenMaterial = new three.ShaderMaterial({
 })
 const screenGeometry = new three.PlaneGeometry(2, 2)
 const screenMesh = new three.Mesh(screenGeometry, screenMaterial)
-scene.add(screenMesh)
+orthographicScene.add(screenMesh)
 
 const wingMaterial = new three.MeshBasicMaterial({map: wingTexture, side: three.DoubleSide})
 const blueMaterial = new three.MeshBasicMaterial({color: 0x0000FF, side: three.DoubleSide})
 const redMaterial = new three.MeshBasicMaterial({color: 0xFF0000, side: three.DoubleSide})
-const wingGeometry = new three.PlaneGeometry(0.2, 0.2)
+const wingGeometry = new three.PlaneGeometry(1, 1)
 const leftWing = new three.Mesh(wingGeometry, wingMaterial)
 const rightWing = new three.Mesh(wingGeometry, wingMaterial)
-scene.add(leftWing)
-scene.add(rightWing)
 
 const butterfly = new three.Object3D()
 butterfly.add(leftWing)
 butterfly.add(rightWing)
-scene.add(butterfly)
-
-const gridHelper = new three.GridHelper(2, 10, 0x00FF00, 0xFFFFFF)
-gridHelper.rotateX(Math.PI / 2)
-// scene.add(gridHelper)
+perspectiveScene.add(butterfly)
 
 const renderer = new three.WebGLRenderer({alpha: true})
 renderer.setSize(vw, vh)
+renderer.autoClear = false
 document.body.appendChild(renderer.domElement)
 
 const targetPosition = new three.Vector3()
@@ -86,27 +86,36 @@ window.addEventListener('resize', handleResize)
 function draw() {
     requestAnimationFrame(draw)
 
+    renderer.clear()
+
     const time = performance.now()
     targetPosition.x = simplex.noise2D(time * 0.00053, 0)
     targetPosition.y = simplex.noise2D(time * 0.00055, 1000)
     const radius = 100 + (simplex.noise2D(time * 0.001, 0) * 5)
 
-    uniforms.time.value = performance.now()
-    uniforms.center.value = butterfly.position
+    vehicle.seek(targetPosition)
+    vehicle.update()
+
+    butterfly.lookAt(new three.Vector3(0, 2, -1).add(targetPosition))
+
+    uniforms.time.value = time
+    uniforms.center.value = vehicle.position
 
     const wingRotation = Math.sin(time * 0.01) * Math.PI * 0.4
 
     leftWing.setRotationFromEuler(new three.Euler(Math.PI / 2, -wingRotation, 0))
     rightWing.setRotationFromEuler(new three.Euler(Math.PI / 2, wingRotation, 0))
 
-    vehicle.seek(targetPosition)
-    vehicle.update()
+    const w = vw * 0.1
+    const h = vh * 0.1
+    const x = ((vehicle.position.x * 0.5) + 0.5) * vw - (w * 0.5)
+    const y = ((-vehicle.position.y * 0.5) + 0.5) * vh - (h * 0.5)
 
-    butterfly.position.x = vehicle.position.x * aspect
-    butterfly.position.y = vehicle.position.y
-    butterfly.lookAt(new three.Vector3(0, 2, -1).add(targetPosition))
+    renderer.setViewport(x, y, w, h)
+    renderer.render(perspectiveScene, perspectiveCamera)
 
-    renderer.render(scene, camera)
+    renderer.setViewport(0, 0, vw, vh)
+    renderer.render(orthographicScene, orthographicCamera)
 }
 
 requestAnimationFrame(draw)
