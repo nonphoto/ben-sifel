4ce9b55aa612ea692072f39521b1964c5910c0f0
Jonas Luebbers <jonaskluebbers@gmail.com>
2018-03-31 01:01:49 -0400

    Make fragment shader more realistic

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -6,6 +6,13 @@ void main()	{
     vec2 coord = (gl_FragCoord.xy / resolution) * 2.0 - 1.0;
     coord.x *= resolution.x / resolution.y;
 
-    float alpha = smoothstep(0.2, 0.3, length(coord - center)) * 0.5;
-    gl_FragColor = vec4(0.0, 0.0, 0.0, alpha);
+    float l = length(coord - center);
+    float glow = (1.0 - smoothstep(0.0, 0.1, l)) * 0.4;
+    float ring = (smoothstep(0.05, 0.2, l) - smoothstep(0.15, 0.2, l)) * 0.1;
+    float dark = smoothstep(0.2, 0.4, l) * 0.7;
+    float vignette = smoothstep(0.2, 2.4, l) * 0.3;
+    float alpha = glow + ring + dark + vignette;
+    float value = 1.0 - step(0.2, l);
+
+    gl_FragColor = vec4(value, value, value, alpha);
 }
